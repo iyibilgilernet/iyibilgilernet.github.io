e4f05c0f08c2b57c5735330f7d575aace35125d2
iyibilgilernet <olmadix@gmail.com>
2026-04-01 13:20:20 +0300

    Fix formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://iyibilgilernet.github.io',
-	base: '/', // Eğer siten ana dizindeyse burası böyle kalsın
+	base: '/',
 });
